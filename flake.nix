44020baeb30344de49317823e42a7d11bbe64667
Kamron Bhavnagri <kamwithk@tuta.io>
2024-11-22 14:37:25 +1100

    Support both X11 and Wayldn

--- flake.nix
+++ flake.nix
@@ -32,9 +32,17 @@
             ];
             RUST_SRC_PATH = rustPlatform.rustLibSrc;
             LD_LIBRARY_PATH = lib.makeLibraryPath [
-              libGL
               libxkbcommon
+              libGL
+
+              # WINIT_UNIX_BACKEND=wayland
               wayland
+
+              # WINIT_UNIX_BACKEND=x11
+              xorg.libXcursor
+              xorg.libXrandr
+              xorg.libXi
+              xorg.libX11
             ];
           };
       }
